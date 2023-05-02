3fd40ce930e84607f1606474783662efffcc014c
u1973238 <u1973238@campus.udg.edu>
2023-05-02 12:32:46 +0200

    Create click_controller.js

---
+++ js/click_controller.js
@@ -0,0 +1,18 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 500,
+    parent: 'game_area',
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: {y: 0},
+			debug: false
+		}
+	},
+    scene: [ CookieScene ]
+};
+
+var game = new Phaser.Game(config);
+
+
