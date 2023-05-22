665497a9dbc269adb9ae61514d9fc6b61f1193db
u1973238 <u1973238@campus.udg.edu>
2023-05-22 16:47:02 +0200

    Update load.js

--- js/load.js
+++ js/load.js
@@ -15,7 +15,7 @@ var load_obj = function(){
 		methods: { 
 			load: function(i){
 				sessionStorage.idPartida = i;
-				loadpage("../html/plataform.html");
+				loadpage("../html/platform.html");
 			}
 		}
 	});
